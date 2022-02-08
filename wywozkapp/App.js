6e2977f47de879d7b474dd24f64c7e8298ad153a
Patryk Nytko <pnytko20@gmail.com>
2022-02-08 13:43:48 +0100

    Tab navigation

--- wywozkapp/App.js
+++ wywozkapp/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Image} from 'react-native';
 import MainPage from "./android/app/src/mainPage";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import homeIcon from "../wywozkapp/android/app/src/images/home.png";
 
-
+const Tab = createBottomTabNavigator();
 const App = () => {
   return (
-      <View>
-          <MainPage />
-      </View>
+      <NavigationContainer>
+          <Tab.Navigator
+              screenOptions={({ route }) => ({
+                  tabBarIcon: () => {
+                      if (route.name === 'Home') {
+                          return <Image source={homeIcon} style={{height: 30, width: 30}} />;
+                      } else if (route.name === 'Settings') {
+                          return <Image source={homeIcon} style={{height: 30, width: 30}} />;
+                      }
+                  },
+              })}
+          >
+              <Tab.Screen name="Home" component={MainPage} options={{ headerShown: false}}/>
+          </Tab.Navigator>
+      </NavigationContainer>
   );
 };
 
