61048f6c25c6405dacb880782bba6353e13f3212
c3bull <kubcor1@gmail.com>
2022-02-08 17:37:33 +0100

    main page icons + tab icons

--- wywozkapp/android/app/src/TabNavigation.js
+++ wywozkapp/android/app/src/TabNavigation.js
@@ -5,7 +5,9 @@ import GarbageSchedule from "./garbageSchedule";
 import MainPage from "./mainPage";
 import homeIcon from "./images/home.png";
 import {Image} from "react-native";
-
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Tab = createBottomTabNavigator();
 
@@ -16,33 +18,39 @@ export default function TabNavigation() {
             screenOptions={({ route }) => ({
                 tabBarIcon: () => {
                     if (route.name === 'Home') {
-                        return <Image source={homeIcon} style={{height: 30, width: 30}} />;
+                        return <FontAwesome color="black" size={40} name="home"/>;
                     } else if (route.name === 'Settings') {
-                        return <Image source={homeIcon} style={{height: 30, width: 30}} />;
+                        return <FontAwesome color="black" size={40} name="home"/>;
                     }
                 },
             })}
         >
             <Tab.Screen name="Settings" component={Settings} options={{
+                tabBarIcon: ({ color }) => (
+                    <MaterialIcons color="black" size={35} name="settings"/>
+                ),
                 title: 'SETTINGS',
                 headerTitleAlign: 'center',
-                tabBarIconStyle: {display: "none"},
-                tabBarLabelStyle: {
-                    fontWeight: "700",
-                    fontSize: 15,
-                    bottom: 15
-                }
+                // tabBarIconStyle: {display: "none"},
+                // tabBarLabelStyle: {
+                //     fontWeight: "700",
+                //     fontSize: 15,
+                //     bottom: 15
+                // }
             }}/>
             <Tab.Screen name="Home" component={MainPage} options={{ headerShown: false}}/>
             <Tab.Screen name="Schedule" component={GarbageSchedule} options={{
+                tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons color="black" size={35} name="timetable"/>
+                ),
                 title: 'GARBAGE SCHEDULE',
                 headerTitleAlign: 'center',
-                tabBarIconStyle: {display: "none"},
-                tabBarLabelStyle: {
-                    fontWeight: "700",
-                    fontSize: 15,
-                    bottom: 15
-                }
+                // tabBarIconStyle: {display: "none"},
+                // tabBarLabelStyle: {
+                //     fontWeight: "700",
+                //     fontSize: 15,
+                //     bottom: 15
+                // }
             }}/>
         </Tab.Navigator>
     );
--- wywozkapp/android/app/src/mainPage.js
+++ wywozkapp/android/app/src/mainPage.js
@@ -3,6 +3,10 @@ import {Text, TouchableOpacity, View, StyleSheet, Image} from 'react-native';
 import locationIcon from "../src/images/locationIcon.png"
 import GetLocation from 'react-native-get-location'
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Entypo from "react-native-vector-icons/Entypo";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 
 const MainPage  = ({navigation}) => {
@@ -32,14 +36,34 @@ const MainPage  = ({navigation}) => {
 
     return (
         <View style={styles.main_buttons_container}>
-            <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('Location')}}><Text style={styles.button_name}>Lokalizacja</Text><Image style={styles.location_icon} source={locationIcon}/>
+            <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('Location')}}><Entypo color="black" size={35} name="location-pin"/><Text style={styles.button_name}>Lokalizacja</Text>
                 <Text style={styles.location_city}>{latitude}, {longitude}</Text><Text style={styles.location_date}>07.02.2022 18:02</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('NextGarbage')}}><Text style={styles.button_name}>Najbliższy  wywóz</Text><Icon name="rocket" /><Text>Sroda, 20.11.2020</Text><Text>Smieci mieszane</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('Contact')}}><Text style={styles.button_name}>Kontakt do firm</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('GarbageSchedule')}}><Text style={styles.button_name}>Harmonogram wywozów</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('RecyclingInfo')}}><Text style={styles.button_name}>Informacje o recyklingu</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('Settings')}}><Text style={styles.button_name}>Ustawienia</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('NextGarbage')}}><MaterialCommunityIcons color="black" size={35} name="dump-truck"/><Text style={styles.button_name}>Najbliższy  wywóz</Text><Text style={styles.button_description}>Sroda, 20.11.2020</Text><Text style={styles.button_description}>Smieci mieszane</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('Contact')}}><AntDesign color="black" size={35} name="contacts"/><Text style={styles.button_name}>Kontakt do firm</Text><Text style={styles.button_description}>Najważniejsze informacje o firmach wywożących odpady</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('GarbageSchedule')}}><MaterialCommunityIcons color="black" size={35} name="timetable"/><Text style={styles.button_name}>Harmonogram wywozów</Text><Text style={styles.button_description}>Daty wywozów {"\n"}odpadów</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('RecyclingInfo')}}><MaterialCommunityIcons color="black" size={35} name="recycle"/><Text style={styles.button_name}>Informacje o recyklingu</Text><Text style={styles.button_description}>Segregacja odpadów</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('Settings')}}><MaterialIcons color="black" size={50} name="settings"/><Text style={styles.button_name}>Ustawienia</Text></TouchableOpacity>
         </View>
+
+    // <View style={styles.main_buttons_container}>
+    //     <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('Location')}}><View style={styles.header}><Entypo color="black" size={27} name="location-pin"/><Text style={styles.button_name}>Lokalizacja</Text></View>
+    //         <Text style={styles.location_city}>{latitude}, {longitude}</Text><Text style={styles.location_date}>07.02.2022 18:02</Text></TouchableOpacity>
+    //     <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('NextGarbage')}}><View style={styles.header}><MaterialCommunityIcons color="black" size={27} name="dump-truck"/><Text style={styles.button_name}>Najbliższy{"\n"} wywóz</Text></View><Text>Sroda, 20.11.2020</Text><Text>Smieci mieszane</Text></TouchableOpacity>
+    //     <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('Contact')}}><View style={styles.header}><AntDesign color="black" size={27} name="contacts"/><Text style={styles.button_name}>Kontakt {"\n"}do firm</Text></View></TouchableOpacity>
+    //     <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('GarbageSchedule')}}><View style={styles.header}><MaterialCommunityIcons color="black" size={27} name="timetable"/><Text style={styles.button_name}>Harmonogram wywozów</Text></View></TouchableOpacity>
+    //     <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('RecyclingInfo')}}><View style={styles.header}><MaterialCommunityIcons color="black" size={27} name="recycle"/><Text style={styles.button_name}>Informacje o recyklingu</Text></View></TouchableOpacity>
+    //     <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('Settings')}}><View style={styles.header}><MaterialIcons color="black" size={27} name="settings"/><Text style={styles.button_name}>Ustawienia</Text></View></TouchableOpacity>
+    // </View>
+
+    //     <View style={styles.main_buttons_container}>
+    //         <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('Location')}}><Entypo color="black" size={27} name="location-pin"/><Text style={styles.button_name}>Lokalizacja</Text>
+    //             <Text style={styles.location_city}>{latitude}, {longitude}</Text><Text style={styles.location_date}>07.02.2022 18:02</Text></TouchableOpacity>
+    //         <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('NextGarbage')}}><MaterialCommunityIcons color="black" size={27} name="dump-truck"/><Text style={styles.button_name}>Najbliższy{"\n"} wywóz</Text><Text style={styles.button_description}>Sroda, 20.11.2020</Text><Text style={styles.button_description}>Smieci mieszane</Text></TouchableOpacity>
+    // <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('Contact')}}><AntDesign color="black" size={27} name="contacts"/><Text style={styles.button_name}>Kontakt {"\n"}do firm</Text><Text style={styles.button_description}>Najważniejsze informacje o firmach wywożących odpady</Text></TouchableOpacity>
+    // <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('GarbageSchedule')}}><MaterialCommunityIcons color="black" size={27} name="timetable"/><Text style={styles.button_name}>Harmonogram wywozów</Text><Text style={styles.button_description}>Daty wywozów {"\n"}odpadów</Text></TouchableOpacity>
+    // <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('RecyclingInfo')}}><MaterialCommunityIcons color="black" size={27} name="recycle"/><Text style={styles.button_name}>Informacje o recyklingu</Text><Text style={styles.button_description}>Segregacja odpadów</Text></TouchableOpacity>
+    //         <TouchableOpacity style={styles.main_buttons} onPress={()=>{navigation.navigate('Settings')}}><MaterialIcons color="black" size={27} name="settings"/><Text style={styles.button_name}>Ustawienia</Text></TouchableOpacity>
+    //     </View>
     );
 };
 
@@ -70,9 +94,10 @@ const styles = StyleSheet.create({
     },
 
     button_name: {
+        float: 'left',
         textAlign: 'center',
         color: 'black',
-        fontSize: 20,
+        fontSize: 19,
         fontFamily: 'Poppins-Bold'
     },
 
@@ -83,11 +108,39 @@ const styles = StyleSheet.create({
 
     location_city: {
         color: 'black',
-        fontFamily: 'Poppins-Medium'
+        fontFamily: 'Poppins-Medium',
+        textAlign: 'center',
     },
 
     location_date: {
         color: 'black',
+        fontFamily: 'Poppins-Medium',
+        textAlign: 'center',
+    },
+
+    header: {
+        justifyContent: "center",
+        alignItems: "center",
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        color: 'black',
         fontFamily: 'Poppins-Medium'
+    },
+    header_icon: {
+        backgroundColor: 'blue',
+        display: 'flex',
+    },
+    header_text: {
+        backgroundColor: 'yellow',
+        width: 'auto'
+    },
+    header_option: {
+        backgroundColor: 'yellow',
+    },
+    button_description: {
+        color: 'black',
+        fontFamily: 'Poppins-Medium',
+        textAlign: 'center'
     },
 });
