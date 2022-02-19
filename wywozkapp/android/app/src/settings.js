c364a4449c5feda046656361e66fc91ad583c402
Patryk Nytko <52817319+Axel2026@users.noreply.github.com>
2022-02-19 13:01:28 +0100

    Poprawki tak jak było

--- wywozkapp/android/app/src/settings.js
+++ wywozkapp/android/app/src/settings.js
@@ -6,6 +6,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import {useTheme} from '@react-navigation/native';
 import {useDispatch, useSelector} from 'react-redux'
+import GetLocation from "react-native-get-location";
 
 const Settings = () => {
 
@@ -19,6 +20,7 @@ const Settings = () => {
     const [onChangeHouseNumber, setOnChangeHouseNumber] = useState(houseNumber);
     const [selectedReminderTime, setSelectedReminderTime] = useState();
     const [pickerModalVisible, setPickerModalVisible] = useState(false);
+    const [inputModalVisible, setInputModalVisible] = useState(false);
     const [locationModalVisible, setLocationModalVisible] = useState(false);
     const [modalData, setModalData] = useState([])
     const [isLocationLoading, setIsLocationLoading] = useState(false)
