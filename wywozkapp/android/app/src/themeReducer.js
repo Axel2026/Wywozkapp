de3382b504893b94ece98584a21c3631a4919f6e
c3bull <kubcor1@gmail.com>
2022-02-19 16:49:46 +0100

    splash screen + ikonka

--- wywozkapp/android/app/src/themeReducer.js
+++ wywozkapp/android/app/src/themeReducer.js
@@ -1,6 +1,10 @@
-const initialState = false
-
+import {AsyncStorage} from "react-native";
+// const value = AsyncStorage.getItem('SELECTED_THEME');
+// const initialState = value
+const initialState = true
 export const themeReducer = (state = initialState, action)=>{
+    console.log('state ' + state)
+    console.log('action ' + JSON.stringify(action))
     if(action.type == 'change_theme'){
         return action.payload
     }
